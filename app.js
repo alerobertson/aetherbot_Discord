d12ada4485e19ac4f457ac409bac1bd60b422348
Alex <xanderjrobertson@gmail.com>
2018-11-26 15:14:22 -0500

    Update to current

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js")
 const client = new Discord.Client()
 const config = require("./config.json")
 const ds = require("./datastore.js")
+
+var howdyVar = 1
  
 module.exports = {
 	wordCheck: wordCheck
@@ -14,7 +16,7 @@ client.on("ready", () => {
 	console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`)
 	client.user.setActivity(`++help`)
 	ds.load_from_disk()
-	setInterval(ds.write_to_disk, 1.08e+7) // every 3 hours
+	setInterval(ds.write_to_disk, 1.8e+6) // every 30 minutes
 });
 
 client.on("guildCreate", guild => {
@@ -41,6 +43,20 @@ client.on('message', msg => {
 	wordCheck(msg)
 });
 
+//Every time a message is edited in a place AetherBot can read
+client.on('messageUpdate', (msgOld, msgNew) => {
+	//Don't read messages from a bot, including AetherBot
+	if(msgNew.author.bot) return 
+	
+	//Format messages outside of guilds (Direct/Group) as: >>username>>: message
+	//Mainly for debugging
+	if(msgNew.channel.type != 'text') {
+		console.log('>>'+msgNew.author.username+'>>: '+msgNew.content)
+	}
+
+	wordCheck(msgNew)
+});
+
 function wordCheck(msg) {
 	//Check for goodbot/badbot
 	goodCheck(msg)
@@ -48,8 +64,42 @@ function wordCheck(msg) {
 	//Check if someone is saying good morning and reward/punish
 	mornCheck(msg)
 	
+	//Howdy
+	howdy(msg)
+	
 	//Check for commands (++command)
 	commandCheck(msg)
+}
+
+function howdy(msg) {
+	var msgCont = msg.content.toLowerCase()
+	msgCont = msgCont.replace(/(<{1}[:][\w]+[:][\w]+>)+/g, '')
+	msgCont = msgCont.replace(/[^0-9a-z]/gi, '')
+	if(msgCont == 'howdy') {
+		howdyVar++
+		if(howdyVar % 10 == 0) {
+			msg.reply("Woah there! That's the " + howdyVar + "th howdy!")
+		}
+	}
+}
+
+function streakCheck(username) {
+	
+}
+
+function yesterdayPoint(username) {
+	let current_day = ds.day_of_year(new Date())
+	
+	let previous_log = ds.morning_log.find(function(entry) {
+		return entry[0] == username && entry[2] == current_day - 1;
+	})
+	
+	if(previous_log) {
+		return previous_log[1]
+	}
+	else {
+		return 0
+	}
 }
 
 function commandCheck(msg) {
@@ -59,6 +109,8 @@ function commandCheck(msg) {
 	//Remove prefix and case down
 	const args = msg.content.slice(config.prefix.length).trim()
 	const command = args.toLowerCase()
+	
+	console.log('>' + msg.author.username +'>: ' + config.prefix + args)
 	
 	switch(command) {
 		case "master":
@@ -79,16 +131,18 @@ function commandCheck(msg) {
 		case "mrank":
 			//Organize data in memory and show it in a simple table
 			let output = "```CURRENT STANDINGS\n\n\n"
+			output += "    Username          Score    Honor\n\n"
 			let rankings = calculateTotalScores()
 			rankings.forEach((person) => {
-				output += ("    " + person.name + " ".repeat(18 - person.name.length) + person.total_score + "\n")
+				output += ("    " + person.name + " ".repeat(18 - person.name.length) + person.total_score +
+					" ".repeat(9 - person.total_score.toString().length) + countEntries(ds.morning_log, person.name) + "\n")
 			})
 			output += "```"
 			
 			msg.reply(output)
 			break
 
-		case "save":
+		/*case "save":
 		
 			ds.write_to_disk()
 			var save = config.saveResponses
@@ -96,15 +150,16 @@ function commandCheck(msg) {
 			
 			msg.reply(saveResponse)
 			break
-		case "load":
+		*/
+		/*case "load":
 			if(msg.author.username != "tacosensei_") {
 				return
 			}
-		
 			ds.load_from_disk()
+
 			msg.reply("Refreshing my memory")
 			break
-			
+		*/	
 		case "mytimezone":
 			var userTimeNames = getUniquePlayerNames(ds.timezones)
 	
@@ -120,7 +175,9 @@ function commandCheck(msg) {
 	if(args.substring(0, 4) == "give" && msg.author.tag == "tacosensei_#3763") {
 		var user = args.slice(4).slice(0, -2)
 		var amount = args.slice(-2)
-		ds.add_entry(user, parseInt(amount))
+		let current_day = ds.day_of_year(new Date())
+		
+		ds.add_entry(user, parseInt(amount), current_day)
 	
 		msg.reply("Yes sir!")
 	}
@@ -160,7 +217,7 @@ function mornCheck(msg) {
 	var mornings = config.mornings;
 	
 	
-	if(mornings.includes(msgCont)) {
+	if(mornings.includes(msgCont) && newDay(msg.author.username)) {
 		//This uses the timezone of the server the bot is running on
 		//Consider changing that timezone if you and the users are from the same timezone
 		var datetime = new Date()
@@ -176,13 +233,26 @@ function mornCheck(msg) {
 			var tardy = config.tardyResponses
 			var tardyResponse = tardy[Math.floor(Math.random()*tardy.length)]
 			msg.channel.send(tardyResponse)
-			ds.add_entry(msg.author.username, -1)
+			ds.add_entry(msg.author.username, -1, ds.day_of_year(new Date()))
 		}
 		else {
-			ds.add_entry(msg.author.username, 1)
+
+			if(yesterdayPoint(msg.author.username) == -1) {
+				msg.reply("good morning! I knew I could count on you!")
+				ds.add_entry(msg.author.username, 2, ds.day_of_year(new Date()))
+			}
+			else {
+				ds.add_entry(msg.author.username, 1, ds.day_of_year(new Date()))
+			}
 		}
 		return;
 	}
+}
+
+function newDay(user) {
+	let current_day = ds.day_of_year(new Date())
+	
+	return !ds.morning_log.find((entry) => entry[0] == user && entry[2] == current_day)
 }
 
 function calculateTimeDisplacement(user) {
@@ -249,8 +319,19 @@ function calculatePlayerScores(data, players) {
 	})
 }
 
+function countEntries(data, name) {
+	let count = 0
+	data.forEach((entry) => {
+		if(entry[0] == name) {
+			count += 1
+		}
+	})
+	return count
+}
+
 function calculateTotalScores() {
 	let playerNames = getUniquePlayerNames(ds.morning_log) //Get all unique names
+	
 	let result = calculatePlayerScores(ds.morning_log, playerNames) //Get all objects 
 	result.sort(function(a, b) {
 		return b.total_score - a.total_score;
